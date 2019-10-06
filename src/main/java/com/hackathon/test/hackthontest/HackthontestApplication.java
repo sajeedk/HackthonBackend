35191e1fdc4a35a12ca1e26f1e607229392f33a1
sajeedk <sajeeddk@gmail.com>
2019-10-06 22:20:31 +0530

    first

---
+++ src/main/java/com/hackathon/test/hackthontest/HackthontestApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.test.hackthontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackthontestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackthontestApplication.class, args);
+	}
+
+}
